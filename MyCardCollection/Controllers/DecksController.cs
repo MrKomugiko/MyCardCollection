be096d23b22a26881e5f04d0a3a34f2e9f0a5254
Kamil Mąka <Mr.Komugiko@gmail.com>
2022-04-26 01:30:10 +0200

    deck detail page, added colored svg and accordion card lists

--- MyCardCollection/Controllers/DecksController.cs
+++ MyCardCollection/Controllers/DecksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyCardCollection.Models;
 using MyCardCollection.Repository;
+using MyCardCollection.ViewModel;
 using Newtonsoft.Json;
 using System.Security.Claims;
 
@@ -22,7 +23,48 @@ namespace MyCardCollection.Controllers
         }
         public async Task<PartialViewResult> Overview(int deckId)
         {
-            return PartialView("_DeckOverview", deckId);
+            var deck = await _deckRepository.GetDeckById(deckId);
+            if (deck == null) return PartialView("_DeckOverview", null);
+            if (! deck.Content.Any()) return PartialView("_DeckOverview", null);
+
+
+            var typeCountGroup = deck.Content
+                .GroupBy(x => x.CardData.Type.Replace("Basic","").Replace("Legendary","").Trim().Split("—")[0].Trim())
+                .Select(g => new
+                {
+                    Key = g.Key,
+                    Value = g.Count()
+                })
+                .OrderByDescending(x=>x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var manaCountGroup = deck.Content
+                .Where(x=>x.CardData.Type.Contains("Land") == false )
+                .GroupBy(x => x.CardData.CMC)
+                .Select(g => new
+                {
+                    Key = g.Key,
+                    Value = g.Count()
+                });
+
+            int[] manaCurveArray = new int[15];
+            foreach (var group in manaCountGroup)
+            {
+                manaCurveArray[(Int32)group.Key] = group.Value;
+            }
+
+            var model = new DeckStatisticsViewModel
+            {
+                DeckId = deckId,
+                DeckName = deck.Name,
+                OwnerId = deck.AppUserId,
+                Size = deck.Content.Sum(x => x.Quantity),
+                ManaCurve = manaCurveArray,
+                TypeDistribution = typeCountGroup,
+                Cards = deck.Content
+                    .Select(x=>(x.Quantity, x.CardData))
+            };
+            return PartialView("_DeckOverview", model);
         }
 
         [HttpGet]
---
+++ MyCardCollection/ViewModel/DeckStatisticsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyCardCollection.Models;
+
+namespace MyCardCollection.ViewModel
+{
+    public class DeckStatisticsViewModel
+    {
+        public int DeckId { get; set; }
+        public string DeckName { get; set; }
+        public string OwnerId { get; set; }
+        public int Size { get; set; }
+        public int[] ManaCurve { get; set; }
+        public Dictionary<string, int> TypeDistribution { get; set; }
+        public IEnumerable<(int Quantity, CardData data)> Cards { get; set; }
+    }
+}
