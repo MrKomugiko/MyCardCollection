f9dd5404317d350946f865071e7f7accb82ab855
Kamil Mąka <Mr.Komugiko@gmail.com>
2022-04-29 20:33:14 +0200

    added user create timestamp

--- MyCardCollection/Models/AppUser.cs
+++ MyCardCollection/Models/AppUser.cs
@@ -4,15 +4,17 @@ namespace MyCardCollection.Models
 {
     public class AppUser : IdentityUser
     {
-        public string Name { get; set; } = "Kamil";
-        public string Lastname { get; set; } = "Mąka";
-        public string City { get; set; } = "Gdańsk";
-        public string CountryCode { get; set; } = "PL";
+        public string Name { get; set; } = "";
+        public string Lastname { get; set; } = "";
+        public string City { get; set; } = "";
+        public string CountryCode { get; set; } = "";
 
         public int UniqueCards { get; set; } = 0;
         public int TotalCards { get; set; } = 0;
         public float TotalValue { get; set; } = 0.0f;
         public int DecksCreated { get; set; } = 0;
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
 
         public ICollection<Deck> Decks { get; set; }    
         public ICollection<CardsCollection> Cards { get; set; }
